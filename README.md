a9052fea97be1a9aa231bd0676ed239da47b974a
Babandeep Singh <er.babandeep@gmail.com>
2021-06-12 18:53:01 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# create-react-bootstrap-app
+
+A CLI to automatically clone the [create-react-bootstrap-boiler](https://github.com/babandeepsingh/create-react-bootstrap-boiler.git).
+
+## Installation
+
+Install the CLI globally OR use npx:
+
+```sh
+npm install -g create-react-bootstrap-app
+```
+
+## Usage
+
+```sh
+# with global install
+create-react-bootstrap-app name-of-app
+# with npx
+npx create-react-bootstrap-app name-of-app
+```
+
+This will create a directory with the given name, clone the [create-react-bootstrap-boiler](https://github.com/babandeepsingh/create-react-bootstrap-boiler.git) repo into it, and install dependencies.
---
+++ index.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+
+const { spawn } = require('child_process');
+
+const name = process.argv[2];
+if (!name || name.match(/[<>:"\/\\|?*\x00-\x1F]/)) {
+  return console.log(`
+  Invalid directory name.
+  Usage: create-react-bootstrap-app app-name  
+`);
+}
+
+const repoURL = 'https://github.com/babandeepsingh/create-react-bootstrap-boiler.git';
+
+runCommand('git', ['clone', repoURL, name])
+  .then(() => {
+    console.log('Done! 🏁');
+    console.log('');
+    console.log('To get started:');
+    console.log('cd', name);
+    console.log('npm install')
+    console.log('npm start');
+  });
+
+function runCommand(command, args, options = undefined) {
+  const spawned = spawn(command, args, options);
+
+  return new Promise((resolve) => {
+    spawned.stdout.on('data', (data) => {
+      console.log(data.toString());
+    });
+
+    spawned.stderr.on('data', (data) => {
+      console.error(data.toString());
+    });
+
+    spawned.on('close', () => {
+      resolve();
+    });
+  });
+}
